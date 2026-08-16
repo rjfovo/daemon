3acdeacef61eef958d38befb0ffd977ce17e61fa
rjfovo <rjf12138@gmail.com>
2026-08-16 12:48:17 +0800

    代码提交

--- translations/en_US.ts
+++ translations/en_US.ts
@@ -4,17 +4,14 @@
 <context>
     <name>AppManager</name>
     <message>
-        <location filename="../src/appmanager.cpp" line="91"/>
         <source>Uninstalling</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/appmanager.cpp" line="111"/>
         <source>Uninstallation failure</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/appmanager.cpp" line="131"/>
         <source>Uninstallation successful</source>
         <translation type="unfinished"></translation>
     </message>
